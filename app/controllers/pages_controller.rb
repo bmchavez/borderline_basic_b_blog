9e2c7c27a10d1ad483c0320c2cfe8d8f869af5f1
Ben Chavez <ben.m.chavez@gmail.com>
2020-06-09 23:58:47 -0500

    dashboard build out

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show] 
+  before_action :set_user, only: [:show]
+
   def about
     @title = 'About Us'
     @content = 'This is the about page'
@@ -7,6 +10,19 @@ class PagesController < ApplicationController
   def dashboard
     @title = 'dashboard'
     @content = 'This your Dashboard!'
+    @posts = policy_scope(Post).order(created_at: :desc)
+    # @post = Post.find(params[:id])
+  end
+  
+
+  private 
+  
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def set_user
+    @user ||= ((User.find(session[:user_id]) if session[:user_id]) || User.new)
   end
 
 end
