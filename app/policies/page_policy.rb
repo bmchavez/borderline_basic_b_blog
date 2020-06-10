d68de0135b215b1166b6759e15024b54d91419c4
Ben Chavez <ben.m.chavez@gmail.com>
2020-06-10 13:41:59 -0500

    page_policy

--- app/policies/page_policy.rb
+++ app/policies/page_policy.rb
@@ -1,9 +1,7 @@
-# def show?
-#   user_is_owner_or_admin?
-# end
-
-# private
-
-# def user_is_owner_or_admin?
-#   record.user == user || user.try(:admin?)
-# end
+class PagePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
