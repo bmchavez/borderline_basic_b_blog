99f06cba5abaed063b755a6da459b45166a8235e
Ben Chavez <ben.m.chavez@gmail.com>
2020-06-11 23:34:11 -0500

    added mailer preview

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,14 @@
 class UserMailer < ApplicationMailer
-
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.user_mailer.welcome.subject
   #
   def welcome
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+    @user = params[:user] # Instance variable => available in view
+    mail(to: @user.email, subject: "Welcome to Borderline Basic B****!")
+    # This will render a view in `app/views/user_mailer`!
   end
 end
+
+
