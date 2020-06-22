01448e097dafd70d7be1bb961691eb3d0f7fc24c
Ben Chavez <ben.m.chavez@gmail.com>
2020-06-22 14:00:51 -0500

    removed the welcome mailer

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,9 +1,9 @@
 class UserMailerPreview < ActionMailer::Preview
-  def welcome
-    user = User.first
+  # def welcome
+  #   user = User.first
     
-    UserMailer.with(user: user).welcome
-  end
+  #   UserMailer.with(user: user).welcome
+  # end
 end
 
 
