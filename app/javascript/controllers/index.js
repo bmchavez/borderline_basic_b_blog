f2672e71d01aa115684092cce74bb30fb5e394ab
Ben Chavez <ben.m.chavez@gmail.com>
2020-06-26 00:15:04 -0500

    ckeditor turbolinks load issue fixed

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,3 +7,7 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
+
+
+var Turbolinks = require("turbolinks")
+Turbolinks.start()
