fdb364383fac561210d386a60aedebb02a1b1ef5
Ben Chavez <ben.m.chavez@gmail.com>
2020-06-26 19:08:28 -0500

    action mailer config settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "borderline_basic_b_blog_production"
 
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
