0b105e6ddaa95a7c0b7f3162d400b5231c838ea7
Ben Chavez <ben.m.chavez@gmail.com>
2020-07-01 15:09:39 -0500

    devise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,7 @@ class PagesController < ApplicationController
   def about
     @title = 'About Us'
     @content = 'This is the about page'
+    @user = User.first
   end
 
   def dashboard
---
+++ test/mailers/previews/new_post_preview.rb
@@ -0,0 +1,16 @@
+class NewPostPreview < ActionMailer::Preview
+  # def welcome
+  #   user = User.first
+    
+  #   UserMailer.with(user: user).welcome
+  # end
+
+  def new_post
+    user = User.first
+    
+    UserMailer.with(user: user).new_post
+  end
+end
+
+
+  
