a1feee9389a487a103ab0a70992c9e99c32cb954
Ben Chavez <ben.m.chavez@gmail.com>
2020-07-01 14:01:27 -0500

    mailer actions

--- app/mailers/new_post_mailer.rb
+++ app/mailers/new_post_mailer.rb
@@ -3,19 +3,15 @@ class NewPostMailer < ApplicationMailer
     # # @post.user = params[:post] # Instance variable => available in view
     # mail(to: "ben.m.chavez@gmail.com", subject: "Borderline Basic B**** Has a New Post!")
     # # This will render a view in `app/views/user_mailer`!
-  
-  
     @users = User.all
-  #   emails = @users.collect(&:email).join(", ")
-    mail(bcc: ["ben.m.chavez@gmail.com", "rosadj.music@gmail.com", "aminchavez.music@gmail.com"], subject: "13yayayaBorderline Basic B**** Has a New Post!")
+    emails = []
+    @users.each { |user| user.subscribed ? emails << user.email : emails }
+    mail(bcc: emails, subject: "Borderline Basic B**** Has a New Post!")
   
 
   # @gibbon = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
   # @audience_id = ENV['MAILCHIMP_AUDIENCE_ID']
   # @gibbon.lists(@audience_id).members.retrieve(params: {"fields": "members.email_address"})
-
-
-
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   after_create :subscribe_to_newsletter
-  after_create :send_welcome_email
+  # after_create :send_welcome_email
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -62,9 +62,9 @@ class User < ApplicationRecord
     end
   end
 
-  def send_welcome_email
-    UserMailer.with(user: self).welcome.deliver_now
-  end
+  # def send_welcome_email
+  #   UserMailer.with(user: self).welcome.deliver_now
+  # end
 
   def subscribe_to_newsletter
     SubscribeToNewsletterService.new(self).call
